ab488000573213769d9ec5c52ca09d4e818b8a31
AbhijitSKshirsagar <abhijitkshirsagar793@gmail.com>
2022-02-15 14:44:50 +0530

    added: Line_Comparision

---
+++ src/com/line_comaprison/Line_Comparision.java
@@ -0,0 +1,10 @@
+package com.line_comaprison;
+
+public class Line_Comparision {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       System.out.println("Welcome Line Coparision Program");
+	}
+
+}
